7fd78906a001a393b0b6cd703a9ec320fba4473f
joao victor <thehoodgamer20@gmail.com>
2022-12-22 18:31:23 -0300

    adicionado o titulo do alertify

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const torresmou = () =>{
 
-    alertify.alert("Você escolheu se transformar no maior bovino do planeta terra. Aquele que rivalizou com boi tatá e bumbá juntos formando um duo contra ele. O boi Henrique Torres que os derrotou fazendo call de banho com a karen  ")
+    alertify.alert("Você escolheu o torresmo!","Você escolheu se transformar no maior bovino do planeta terra. Aquele que rivalizou com boi tatá e bumbá juntos formando um duo contra ele. O boi Henrique Torres que os derrotou fazendo call de banho com a karen  ")
 
     const audio = document.querySelector("#audioTorresmo")
     audio.play()
